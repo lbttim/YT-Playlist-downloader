3c4544fa4cc57ff4127e1cfdc435b072c2150e19
TTIMLB <tim.lajevec@gmail.com>
2022-10-05 08:47:08 +0200

    Add files via upload

---
+++ download.py
@@ -0,0 +1,20 @@
+import yt_dlp
+from pytube import Playlist
+
+p = Playlist('https://www.youtube.com/playlist?list=')
+
+for url in p.video_urls:
+    print(url)
+    video_url = url
+    video_info = yt_dlp.YoutubeDL().extract_info(url = video_url,download=False)
+    filename = f"{video_info['title']}.mp3"
+    options={
+        'format':'bestaudio/best',
+        'keepvideo':False,
+        'outtmpl':filename,
+    }
+
+    with yt_dlp.YoutubeDL(options) as ydl:
+        ydl.download([video_info['webpage_url']])
+
+    print("Download complete... {}".format(filename))
